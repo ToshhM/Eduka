e333c1f7de4bb6b794f6d2bfefc594ac005c692d
Antoine Durussel <antoine.durussel@gmail.com>
2020-10-15 12:47:28 +0200

    update existant pages

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php require('template/header.php'); ?>
 
-<div class="home">
-    <div class=image>
-        <h1> Mascotte</h1>
-    </div>
-    <div class="logo">
-        <h1> LOGO</h1>
-        <img src="assets/image/logo.png" alt="Eduka">
+<div class="anim">
+    <div class="anim-container" onclick="transitionToPage('accueil.php')">
+        <img src="assets/image/anim.jpg" alt="Tom" class="d-block h-75 w-100">
+        <img src="assets/image/anim-logo.png" alt="Eduka">
     </div>
 </div>
 
--- template/navbar.php
+++ template/navbar.php
@@ -1,8 +1,16 @@
-<div class="Navbar">
-        <ul>
-            <li><img src="assets/image/book.png"></li>
-            <li><img src="assets/image/Tracé 2.png"></li>
-            <li><img src="assets/image/profile.png"></li>
-            <li><img src="assets/image/settings.png"></li>
-        </ul>
+<div class="navbar">
+    <div>
+        <a href="histoire.php">
+        <img src="assets/image/<?php if(isset($page) && $page == "histoire") { echo 'book-active.png'; } else { echo 'book.png'; } ?>">
+        </a>
     </div>
+    <div>
+        <img src="assets/image/Tracé 2.png">
+    </div>
+    <div>
+        <img src="assets/image/profile.png">
+    </div>
+    <div>
+        <img src="assets/image/settings.png">
+    </div>
+</div>
