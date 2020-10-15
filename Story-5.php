f63fe0806ed3e1141682b9c155f6d0ae70d9dd2e
ToshhM <49293384+ToshhM@users.noreply.github.com>
2020-10-15 16:13:59 +0200

    Story + Images
    Création des storys 4 5 8 9 ... problème avec les images

---
+++ Story-5.php
@@ -0,0 +1,21 @@
+<?php require('template/header.php'); ?>
+
+<div class="home">
+    <?php require('template/navbar.php'); ?>
+    <div class="story-container">
+        <h2 class="my-3">Le chat et le poisson</h2>
+        <div class="main-container">
+            <div class="img-container">
+                <img src="assets/image/story-1/vignette-3.png" class="rounded d-block mx-auto mb-3" alt="Tom">
+            </div>
+            <div class="text-container">
+                <p>Il lui faudrait quelque 
+                chose pour pouvoir 
+            les pêcher. </p>
+            </div>
+        </div>
+        <a href="story-5.php" class="btn-story btn btn-warning mt-3 w-50">OUI !</a>
+    </div>
+</div>
+
+<?php require('template/footer.php');?>
---
+++ story-8.php
@@ -0,0 +1,22 @@
+<?php require('template/header.php'); ?>
+
+<div class="home">
+    <?php require('template/navbar.php'); ?>
+    <div class="story-container">
+        <h2 class="my-3">Le chat et le poisson</h2>
+        <div class="main-container">
+            <div class="img-container">
+                <img src="assets/image/story-1/vignette-3.png" class="rounded d-block mx-auto mb-3" alt="Tom">
+            </div>
+            <div class="text-container">
+                <p>Mais Miy le chat
+arrive et lui aussi a 
+besoin de ton aide. 
+</p>
+            </div>
+        </div>
+        <a href="story-9.php" class="btn-story btn btn-warning mt-3 w-50">Suivant !</a>
+    </div>
+</div>
+
+<?php require('template/footer.php');?>
