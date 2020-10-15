4fd238cf5692e7f1571d5c498111e4e0d865173f
Antoine Durussel <antoine.durussel@gmail.com>
2020-10-15 16:45:49 +0200

    feat augmented reality

--- template/navbar.php
+++ template/navbar.php
@@ -1,11 +1,13 @@
 <div class="navbar">
     <div>
         <a href="histoire.php">
-        <img src="assets/image/<?php if(isset($page) && $page == "histoire") { echo 'book-active.png'; } else { echo 'book.png'; } ?>">
+            <img src="assets/image/<?php if(isset($page) && $page == "histoire") { echo 'book-active.png'; } else { echo 'book.png'; } ?>">
         </a>
     </div>
     <div>
-        <img src="assets/image/Tracé 2.png">
+        <a href="trophees.php">
+            <img src="assets/image/<?php if(isset($page) && $page == "trophees") { echo 'truc-active.png'; } else { echo 'truc.png'; } ?>">
+        </a>
     </div>
     <div>
         <img src="assets/image/profile.png">
