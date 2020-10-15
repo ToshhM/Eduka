4840396ac9e84123e00a69b4e74a4aa1233dbcb4
ToshhM <49293384+ToshhM@users.noreply.github.com>
2020-10-15 15:08:34 +0200

    Trophee

---
+++ trophee.php
@@ -0,0 +1,11 @@
+<?php require('template/header.php'); ?>
+
+<div class="background">
+    <?php require('template/navbar.php'); ?>
+    <div class="trophee"> 
+    <h1> Tes trophés ! </h1>
+    
+</div>
+
+
+<?php require('template/footer.php'); ?>
