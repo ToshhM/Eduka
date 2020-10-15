1098540d92020ae5797d0339c3604d23ea57dff1
ToshhM <49293384+ToshhM@users.noreply.github.com>
2020-10-15 12:48:05 +0200

    cat.php

---
+++ cat.php
@@ -0,0 +1,21 @@
+<?php require('template/header.php'); ?>
+
+<div class="home"> 
+
+
+<h2> le chat et le poisson </h2>
+    <div class="images"></div>
+    <div class="text">
+    <p>Salut, moi c’est Miy.  <br>
+    J’aurai besoin de toi,  <br>
+    j'ai faim ! <br>
+    Tu peux m’aider ? </p>
+
+
+</div>
+
+
+
+
+
+<?php require('template/footer.php');?>
